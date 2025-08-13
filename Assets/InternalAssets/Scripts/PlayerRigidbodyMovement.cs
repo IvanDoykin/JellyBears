62063e8d3000075387cb458eb5e1b7f93552c96d
Ivan <52855695+IvanDoykin@users.noreply.github.com>
2025-08-13 18:37:32 +0300

    4

---
+++ Assets/InternalAssets/Scripts/PlayerRigidbodyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerRigidbodyMovement : Movement
+{
+    [SerializeField] private Rigidbody _body;
+
+    protected override void Move()
+    {
+        _body.velocity = _speed * transform.forward;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _body.velocity += _speed * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _body.velocity += _speed * Vector3.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+}

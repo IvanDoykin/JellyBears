00e75d6c6288acb1c8fc58841011d248de26524b
Ivan <52855695+IvanDoykin@users.noreply.github.com>
2025-08-16 13:35:23 +0300

    6.4 - 6.5

--- Assets/_InternalAssets/Scripts/ForwardMovement.cs
+++ Assets/_InternalAssets/Scripts/ForwardMovement.cs
@@ -1,9 +1,41 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class ForwardMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _startSpeed = 10.0f;
+    [SerializeField] private float _pushForce = 10f;
     [SerializeField] private Rigidbody _body;
+
+    private float _currentSpeed = 0f;
+    private bool _hasPushed = false;
+
+    private void Start()
+    {
+        GetComponent<ObstacleCollisionDetector>().HasCollided += PushBackward;
+    }
+
+    private void PushBackward()
+    {
+        StartCoroutine(SmoothPushBackward());
+    }
+    
+    private IEnumerator SmoothPushBackward()
+    {
+        _hasPushed = true;
+        _currentSpeed = 0f;
+
+        _body.AddForce(-transform.forward * _pushForce, ForceMode.Impulse);
+
+        float counter = 0f;
+        while (counter < 1f)
+        {
+            counter += Time.deltaTime;
+            yield return null;
+        }
+
+        _hasPushed = false;
+    }    
 
     private void FixedUpdate()
     {
@@ -12,6 +44,16 @@ public class ForwardMovement : MonoBehaviour
 
     private void Move()
     {
-        _body.velocity = _speed * transform.forward;
+        if (_hasPushed)
+        {
+            return;
+        }
+
+        if (_currentSpeed < _startSpeed)
+        {
+            _currentSpeed += _startSpeed * Time.deltaTime;
+        }
+
+        _body.velocity = _currentSpeed * transform.forward;
     }
 }
---
+++ Assets/_InternalAssets/Scripts/ObstacleCollisionDetector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+public class ObstacleCollisionDetector : MonoBehaviour
+{
+    public event Action HasCollided;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetComponentInParent<Obstacle>() != null)
+        {
+            HasCollided?.Invoke();
+        }
+    }
+}
